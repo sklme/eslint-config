4f41e82a22c31a43bb0eb4a4e930da368417d758
wrynnsun <wrynnsun@tencent.com>
2023-01-30 16:30:48 +0800

    fix: 解决没有输出的问题

--- pugin.cjs
+++ pugin.cjs
@@ -1,4 +1,11 @@
+// base
 const base = require("./base/base.cjs");
+
+// ts
+const tsBase = require("./ts/ts-base.cjs");
+const tsTypecheck = require("./ts/ts-typecheck.cjs");
+
+// vue
 const vue = require("./vue/vue.cjs");
 const vueTS = require("./vue/vue-ts.cjs");
 const vueTSTypecheck = require("./vue/vue-ts-typecheck.cjs");
@@ -6,6 +13,8 @@ const vueTSTypecheck = require("./vue/vue-ts-typecheck.cjs");
 module.exports = {
   configs: {
     base,
+    "ts-base": tsBase,
+    "ts-typecheck": tsTypecheck,
     vue,
     "vue-ts": vueTS,
     "vue-ts-typecheck": vueTSTypecheck,
