c337d1866618340afc8d8710329ce12e4510fac7
wrynnsun <wrynnsun@tencent.com>
2023-01-30 19:31:32 +0800

    fix: add tsconfig

--- ts/ts-typecheck.cjs
+++ ts/ts-typecheck.cjs
@@ -3,6 +3,11 @@ module.exports = {
     require.resolve("./ts-base.cjs"),
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
   ],
+  parserOptions: {
+    project: "tsconfig.json",
+  },
+  // 对所有的js文件，忽略project设置
+  // 并且给env node，因为这些文件一般都是构建配置，会使用的node能力
   overrides: [
     {
       files: ["*.js", "*.cjs", "*.mjs"],
